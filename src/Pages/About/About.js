179013ea138c0ce8d96498e9fcf6e3127faa844f
Jon McCutchen <jmmccutchen1@gmail.com>
2021-11-04 15:58:51 +0100

    add contact and about pages

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 
 export const About = () => {
     return(
-        <h1>Hi, I'm <span>Jon</span></h1>
+        <Container fluid={true} className='mt-4'>
+            <h2 className='text-center'>Hi, I'm <span className='text-primary'>Jon</span></h2>
+            <br />
+            <p className='mb-5 mx-auto w-xl-50'>I'm a small town kid from South Carolina that got the rare opportunity to travel the world. I got a degree in mechanical engineering while spending 3 years in London, England. During that time, I taught myself html, css, javascript and react.js with a little help from Codecademy. Afterwards I decided that I preferred working with computers and the web. I like the modernity of the field allowing me to work in remote teams of the best developers around the world. I have a passion for traveling and absorbing new places and cultures so tech fits well with my personality. I'd love to start my own company one day, something that has a positive impact on the planet and the people living on it. For now I want to work for an inspiring company and bring as much value as I can while building my skills and experience.</p>
+        </Container>
     )
 }
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+const linkedIn = <FontAwesomeIcon icon={faLinkedin} size='lg'/>
+const github = <FontAwesomeIcon icon={faGithub} size='lg'/>
+const instagram = <FontAwesomeIcon icon={faInstagram} size='lg' />
+const facebook = <FontAwesomeIcon icon={faFacebook} size='lg' />
+const twitter = <FontAwesomeIcon icon={faTwitter} size='lg' />
+
 
 export const Contact = () => {
     return(
-        <h1>Contact works</h1>
+        <Container className='text-center contact-container'>
+            <p className='my-5'>Contact me via email at: <a className='text-decoration-none' href='mailto:jmmccutchen1@gmail.com'>jmmccutchen1@gmail.com</a></p>
+            <h5 className='pt-5 mb-5'>Socials</h5>
+            <a href='https://www.linkedin.com/in/jonmccutchen/' target='_blank' rel='noreferrer'>{linkedIn}</a>
+            <a href='https://github.com/JonMcCutchen' target='_blank' rel='noreferrer'>{github}</a>
+            <a href='https://www.instagram.com/jon_mccutchen/' target='_blank' rel='noreferrer'>{instagram}</a>
+            <a href='https://www.facebook.com/profile.php?id=100010600638652' target='_blank' rel='noreferrer'>{facebook}</a>
+            <a href='https://twitter.com/Jon_McCutchen' target='_blank' rel='noreferrer'>{twitter}</a>
+        </Container>
     )
 }
